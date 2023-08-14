975ca50d38f662379e9cfc6034f2ca407f285033
ishaantri <129971150+ishaantri@users.noreply.github.com>
2023-08-14 14:20:19 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup.py
 PEP 621 switches most of Packaging to `pyproject.toml` -- yet keep a "dummy" setup.py for external code that has not
 yet upgraded.
 """
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name="voltron", packages=["voltron"])
+setup(name="voltron", packages=find_packages(where='.', exclude=['cache'],), package_dir={"": "."})
